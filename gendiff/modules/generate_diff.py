cf9aaf5005bd20ce83c87da88ae8d68fc5a5d57a
ezhov_dv <alldost@bk.ru>
2024-05-27 19:16:53 +0500

    change generate diff and stylish

--- gendiff/modules/generate_diff.py
+++ gendiff/modules/generate_diff.py
@@ -2,36 +2,28 @@ from gendiff.modules.parse import parse
 
 
 def generate_diff(file1, file2):
-    same = dict()
-    diff1 = dict()
-    diff2 = dict()
+    diff = dict()
     dict1, dict2 = parse(file1, file2)
 
     for key in set(dict1) | set(dict2):
 
         if dict1.get(key) == dict2.get(key):
-            same[key] = dict1[key]
+            diff["  " + key] = dict1[key]
 
         else:
-            diff1[key] = dict1.get(key)
-            diff2[key] = dict2.get(key)
+            if key in dict1:
+                diff["- " + key] = dict1.get(key)
+            if key in dict2:
+                diff["+ " + key] = dict2.get(key)
 
-    return stylish(same, diff1, diff2)
+    return stylish(diff)
 
 
-def stylish(same, diff1, diff2):
+def stylish(diff):
     result = []
 
-    for key, value in same.items():
-        result.append(f'    {key}: {value}'.lower())
-
-    for key, value in diff1.items():
-        if value is not None:
-            result.append(f'  - {key}: {value}'.lower())
-
-    for key, value in diff2.items():
-        if value is not None:
-            result.append(f'  + {key}: {value}'.lower())
+    for key, value in diff.items():
+        result.append(f'  {key}: {value}'.lower())
 
     result = sorted(result, key=lambda x: x[4:5])
 
