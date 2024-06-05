6e2a07f33bfc871c937ef843779187355f76a3c6
ezhov_dv <alldost@bk.ru>
2024-06-05 21:14:41 +0500

    step 6 try 5

--- gendiff/modules/generate_diff.py
+++ gendiff/modules/generate_diff.py
@@ -2,57 +2,93 @@ from gendiff.modules.parse import parse
 
 
 def generate_diff(file1, file2, formater=None):
-    diff = dict()
+    diff = []
     dict1, dict2 = parse(file1, file2)
     keys = sorted(set(dict1) | set(dict2))
+
+    def walk(dict_):
+        result = []
+        for key, value in dict_.items():
+            entry_ = {'key': key}
+            if not isinstance(value, dict):
+                result.append({'key': key, 'value': value})
+            else:
+                entry_['children'] = walk(value)
+                result.append(entry)
+        return result
 
     for key in keys:
         value1 = dict1.get(key)
         value2 = dict2.get(key)
 
+        entry = {'key': key}
+
         if isinstance(value1, dict) and isinstance(value2, dict):
-            diff[key] = generate_diff(value1, value2)
+            entry['status'] = 'unchanged'
+            entry['children'] = generate_diff(value1, value2)
+            diff.append(entry)
+
+        elif isinstance(value1, dict) and not isinstance(value2, dict):
+            entry['status'] = 'removed'
+            entry['children'] = walk(value1)
+            diff.append(entry)
+
+        elif not isinstance(value1, dict) and isinstance(value2, dict):
+            entry['status'] = 'added'
+            entry['children'] = walk(value2)
+            diff.append(entry)
 
         else:
             if value1 == value2:
-                diff[key] = value1
+                diff.append({'key': key, 'status': 'unchanged', 'value': value1})
             else:
                 if key in dict1:
-                    diff["- " + key] = value1
+                    diff.append({'key': key, 'status': 'removed', 'value': value1})
                 if key in dict2:
-                    diff["+ " + key] = value2
-
-    if formater is None:
-        formater = stylish
-
+                    diff.append({'key': key, 'status': 'added', 'value': value2})
     # return diff
-    return formater(diff)
+    return stylish(diff)
 
 
-def stylish(diff_dict, level=1):
+def stylish(diff_list, level=1):
     result = ''
 
-    def stylish_level(value, current_level):
+    def stylish_level(list_, current_level):
         level_result = ''
 
-        if not isinstance(value, dict):
-            return str(value)
+        # if not isinstance(list_, dict):
+        #    return str(list_)
 
-        else:
-            spaces_number = 4
+        spaces_number = 4
+
+        if not isinstance(list_, list):
+            level_result += str(list_)
+
+        if isinstance(list_, list):
+
             level_result += '{\n'
 
-            for key, value in value.items():
+            for item in list_:
+                status = item.get('status')
+                key = item.get('key')
 
-                if key.startswith('+') or key.startswith('-'):
-                    level_result += f'{(current_level * spaces_number - 2) * " "}{key}: '
-                else:
+                if status == 'unchanged' or status is None:
                     level_result += f'{current_level * spaces_number * " "}{key}: '
-                level_result += stylish_level(value, current_level + level) + '\n'
-            level_result += f'{spaces_number * (current_level - level) * " "}' + '}'
+                elif status == 'removed':
+                    level_result += f'{(current_level * spaces_number - 2) * " "}- {key}: '
+                elif status == 'added':
+                    level_result += f'{(current_level * spaces_number - 2) * " "}+ {key}: '
 
+                if 'children' in item:
+                    level_result += stylish_level(item['children'], current_level + level) + '\n'
+                else:
+                    level_result += str(item['value']) + '\n'
+                # else:
+                #    level_result += f'{current_level * spaces_number * " "}{item}'
+
+            level_result += f'{spaces_number * (current_level - level) * " "}' + '}'
         return level_result
 
-    result += stylish_level(diff_dict, level)
+    result += stylish_level(diff_list, level)
 
     return result
